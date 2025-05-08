d9b98b4082a5a02d257343e22c47332d6ae5aa94
isaaclins <isaac.lins07@gmail.com>
2025-05-08 07:45:43 +0200

    Update dev.sh to start Docker daemon before building and running the container, and include RUN_TESTS_ARG in the build command.

--- dev.sh
+++ dev.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+#first, start docker daemon
+open -a Docker && while ! docker info > /dev/null 2>&1; do sleep 1; done
+
+
 # First, delete the container if it exists
 docker rm -f home-server 2>/dev/null
 
 # Then, build the image
-docker build -t home-server .
+docker build -t home-server $RUN_TESTS_ARG .
 
 # Then, run the container
 docker run -it -p 8000:8000 home-server
